5aba0f2cd94d1f26ce85f4b64decfdc88385703e
Quentin <pamartqu@gmail.com>
2022-01-05 23:12:26 +0100

    preparation projet cpp (export lib)

--- ChessAnalytics/main.cpp
+++ ChessAnalytics/main.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+extern "C"{
+    //__declspec(dllexport) MyMLP * createMLPModel(int npl[], int nplSize) {}
 }
