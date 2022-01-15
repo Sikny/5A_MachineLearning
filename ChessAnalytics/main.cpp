82f6aca1596e3f02a28ba79191b1c062abfa4b39
Quentin <pamartqu@gmail.com>
2022-01-15 15:26:37 +0100

    protos mlp

--- ChessAnalytics/main.cpp
+++ ChessAnalytics/main.cpp
@@ -1,27 +1,55 @@
 #include "Eigen/Dense"
 
-extern "C"{
-    __declspec(dllexport) double* predictLinearModelRegression(double sampleInputs[],
-               double sampleExpectedOutputs[], int sampleCount, int inputDim,
-               int outputDim){
-        double* result = new double[inputDim+1];
+struct MLP {
+    
+};
+
+extern "C" {
+    __declspec(dllexport) double *predictLinearModelRegression(double sampleInputs[],
+                                                               double sampleExpectedOutputs[], int sampleCount,
+                                                               int inputDim,
+                                                               int outputDim) {
+        double *result = new double[inputDim + 1];
 
         Eigen::MatrixXd X(inputDim, sampleCount);
         Eigen::MatrixXd Y(inputDim, sampleCount);
-        for(int i = 0; i < sampleCount; ++i){
-            for(int j = 0; j < inputDim; ++j){
+        for (int i = 0; i < sampleCount; ++i) {
+            for (int j = 0; j < inputDim; ++j) {
                 X(i, j) = sampleInputs[i * inputDim + j];
             }
         }
-        for(int i = 0; i < sampleCount; ++i){
-            for(int j = 0; j < outputDim; ++j){
+        for (int i = 0; i < sampleCount; ++i) {
+            for (int j = 0; j < outputDim; ++j) {
                 Y(i, j) = sampleExpectedOutputs[i * outputDim + j];
             }
         }
         Eigen::MatrixXd W = ((X.transpose() * X).inverse() * X.transpose()) * Y;    // vector
-        for(int i = 0; i < inputDim+1; ++i){
+        for (int i = 0; i < inputDim + 1; ++i) {
             result[i] = W(i, 0);
         }
         return result;
+    }
+
+    __declspec(dllexport) MLP *createMlpModel(int npl[], int nplSize) {
+        auto model = new MLP();
+        // todo
+        return model;
+    }
+
+    _declspec(dllexport) void trainModel(MLP *model, double samplesInputs[], double samplesExpectedOutputs[], int sampleCount, int inputDim,
+                    int outputDim, double alpha, int nbIter) {
+        // todo
+    }
+
+    __declspec(dllexport) double *predictMlpModelRegression(MLP *model, double sampleInputs[], int inputDim) {
+        // todo
+    }
+
+    __declspec(dllexport) void destroyMlpModel(MLP *model) {
+        delete model;
+    }
+
+    __declspec(dllexport) void destroyMlpResult(const double *result) {
+        delete[] result;
     }
 }
