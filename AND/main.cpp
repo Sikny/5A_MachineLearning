106cc41d6b9478af15a0ff2eb3524931ceb6950b
Ronan <r.dhersignerie@gmail.com>
2022-02-15 02:46:36 +0100

    Update main.cpp

--- AND/main.cpp
+++ AND/main.cpp
@@ -5,7 +5,8 @@
 #include "src/MoveMapper.h"
 int main() {
     Network net;
-    net.addLayer(2);
+    net.addLayer(10);
+    net.addLayer(5);
     net.addLayer(1);
 
     DataSet set;
@@ -42,27 +43,26 @@ int main() {
     // fin data
 
 
-    //And dataset
-    /*set.addData({0, 0}, {0});
-    // in : {d4,d5, c4, c6}, {0}
-
-    // in : {0, 5, 9, 12}, {0}
-    //
-    //in : {d4,d5, c4, c6}
-    set.addData({0, 0}, {0});
-    set.addData({1, 0}, {0});
-    set.addData({0, 1}, {0});
-    set.addData({1, 1}, {1});*/
 
     net.train(set);
 
     DataSet set2;
-    //other dataset
-    set2.addData({0.8, 0.9}, {1});
-    set2.addData({0, 0.5}, {0});
-    set2.addData({0.7, 0}, {0});
-    set2.addData({0, 0}, {0});
-
+    // data 1
+    moves = parseMoves("c4 c6 e4 e6 Nf3 d5 cxd5 cxd5 e5 Nc6 d4 Qb6 Be3 Nge7 b3 Nf5 Bg5 Bb4+ Nfd2 Ncxd4 a3 Bxd2+ Nxd2 Bd7 Bd3 O-O O-O h6 Bf6 Kh7 Nf3 gxf6 exf6 Bb5 Ng5+ hxg5 Qh5+ Kg8 Qxg5+ Kh7 Qg7#");
+    for(auto& move : moves){
+        float value = mapper.registerMove(move);
+        inData.push_back(value);
+    }
+    set2.addData(inData, {1});   // white, 0 si black
+    // fin data
+    // data 2
+    moves = parseMoves("e4 c6 d4 d5 Nc3 dxe4 Nxe4 Bf5 Bd3 Nd7 Nc3 Bxd3 Qxd3 e6 Nf3 Ngf6 Bg5 h6 Bh4 Be7 Bg3 a5 O-O a4 Ne5 O-O a3 c5 Rad1 Nb6 Qb5 cxd4 Nxa4 Nxa4 Qxb7 Nc5 Qf3 Na4 Nc6 Qd7 Rxd4 Nd5 Ne5 Qb5 Rfd1 Nxb2 Rb1 Bc5 Rh4 Rxa3 Qg4 Nc3 Re1 Nbd1 Qxg7+ Kxg7 Rg4+ Kh7 Rh4 Ne2+ Kf1 Nxg3+");
+    for(auto& move : moves){
+        float value = mapper.registerMove(move);
+        inData.push_back(value);
+    }
+    set2.addData(inData, {0});   // white, 0 si black
+    // fin data
     float error = net.evaluate(set2,0.25);
     std::cout << set.Inputs().size() <<std::endl;
     std::cout << set.ExpectedOutput().size() <<std::endl;
