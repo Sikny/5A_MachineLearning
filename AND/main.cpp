e704e10178870ae83de25265ab187e2b4747cd9f
Aedrinios <clementclerc2@gmail.com>
2022-02-15 01:59:33 +0100

    display result

--- AND/main.cpp
+++ AND/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 #include "src/Network.h"
 #include "src/DataSet.h"
+
+void display(DataSet &set) {
+    for (int i = 0; i < set.Inputs().size(); ++i) {
+        std::cout << "-----Input-----" << std::endl;
+        std::cout << "{";
+        for (int j = 0; j < set.Inputs()[i].size(); ++j) {
+            std::cout << set.Inputs()[i][j];
+            if (j != set.Output()[i].size()) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "}";
+        std::cout << std::endl;
+
+        std::cout << "-----Expected Output-----" << std::endl;
+        std::cout << "{";
+        for (int j = 0; j < set.ExpectedOutput()[i].size(); ++j) {
+            std::cout << set.ExpectedOutput()[i][j];
+            if (j != set.ExpectedOutput()[i].size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "}";
+        std::cout << std::endl;
+
+        std::cout << "-----Output-----" << std::endl;
+        std::cout << "{";
+        for (int j = 0; j < set.Output()[i].size(); ++j) {
+            std::cout << set.Output()[i][j];
+            if (j != set.Output()[i].size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "}";
+        std::cout << std::endl << std::endl;
+    }
+}
+
 int main() {
     Network net;
     net.addLayer(2);
@@ -20,7 +58,6 @@ int main() {
     set.addData({0, 0.5}, {0});
     set.addData({0.7, 0}, {0});
     set.addData({0, 0}, {0});
-
 
     return 0;
 }
